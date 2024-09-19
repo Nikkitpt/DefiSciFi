f7a38490f419d4df94cfed85157a9b085cb5e711
nikkitpt <ndavis213@gmail.com>
2024-09-19 18:32:49 -0400

    removed transaction mock data

--- frontend/src/components/TransactionHistory.js
+++ frontend/src/components/TransactionHistory.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from '@mui/material';
-import { mockTransactions } from '../mockData';
+import axios from 'axios'; // Make sure to install axios with `npm install axios`
 
 function TransactionHistory() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:5000/transactions');
+        setTransactions(response.data);
+      } catch (error) {
+        console.error('Error fetching transactions:', error);
+        // Optionally handle errors, e.g., by setting an error state or displaying a notification
+      }
+    };
+
+    fetchTransactions();
+  }, []); // The empty dependency array ensures this effect runs only once after the component mounts
+
   return (
     <TableContainer style={{ marginBottom: '20px', marginTop: '20px' }} component={Paper}>
       <Typography variant="h5" style={{ margin: '16px' }}>Transaction History</Typography>
@@ -17,7 +33,7 @@ function TransactionHistory() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {mockTransactions.map((transaction, index) => (
+          {transactions.map((transaction, index) => (
             <TableRow key={transaction.id} style={{ backgroundColor: index % 2 ? '#f9f9f9' : 'white' }}>
               <TableCell>{transaction.type}</TableCell>
               <TableCell>{transaction.amount}</TableCell>
