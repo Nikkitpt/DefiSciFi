728dd3bde40d76d0bacf0ef2280e26fd3af10419
nikkitpt <ndavis213@gmail.com>
2024-09-23 17:33:57 -0400

    fixing procfile

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -11,8 +11,8 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Initialize the extensions with the app instance
     # db.init_app(app)
